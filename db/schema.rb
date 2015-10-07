8094ac57d7ca85c0cca3ac48a71ef4c0b191d1c0
Thomas Ahn <thomas.ahn@touchworks.co.kr>
2015-10-07 15:53:10 +0900

    data seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007014901) do
+ActiveRecord::Schema.define(version: 20151007054947) do
+
+  create_table "bulletins", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.string   "post_type",   default: "bulletin"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "bulletin_id"
+    t.string   "picture"
   end
+
+  add_index "posts", ["bulletin_id"], name: "index_posts_on_bulletin_id"
 
 end
